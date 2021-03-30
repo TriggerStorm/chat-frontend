dea8761f816dc73bb5a4106086ef126b35801d0a
TriggerStorm <michael.h.p@hotmail.com>
2021-03-30 13:47:40 +0200

    Update stock.module.ts

--- src/app/stock/stock.module.ts
+++ src/app/stock/stock.module.ts
@@ -4,13 +4,14 @@ import { StockComponent } from './stock.component';
 import {StockCreateComponent} from './stock-create/stock-create.component';
 import {BrowserModule} from '@angular/platform-browser';
 import {ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 const routes: Routes = [
   { path: '', component: StockComponent },
   { path: 'create', component: StockCreateComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes), ReactiveFormsModule],
+  imports: [RouterModule.forChild(routes), ReactiveFormsModule, CommonModule],
   exports: [RouterModule],
   declarations: [StockCreateComponent, StockComponent]
 })
